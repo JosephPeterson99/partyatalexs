a755e99502f067a894f75432f856f1077fbaf8c4
joseph <josephkamphuis@gmail.com>
2024-11-13 23:03:16 -0600

    initial work

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Party at Alex's</title>
+    <link rel="icon" type="image/x-icon" href="./favicon.png">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <style> <?php include 'style.css';?> </style>
+</head>
+
+<body>
+    <div class="bg-grid">
+        <div class="container main-wrapper">
+            <div class="logo-cell"><img src="./img/logo.png" /></div>
+            <br><br>
+            <div class="section-heading">
+                <h2>Live Map</h2>
+                <div class="wide-cell">
+                    <iframe class="server-map" src="http://51.81.194.24:21744/"></iframe>
+                </div>
+            <div class="section-heading">
+                <h2>Recommended Resource Pack</h2>
+            </div>
+            <div class="wide-cell"><img src="./img/pack.png" /></div>
+            <br><br>
+            <div class="section-heading">
+                <h2>Server Data Packs</h2>
+                <!-- <h4>(More info on vanillatweaks.net)</h4> -->
+            </div>
+            <div class="container">
+                <div class="row pack-row"><div class="wide-cell"><img src="./img/data/armored-elytra.png" /></div></div>
+                <div class="row pack-row"><div class="wide-cell"><img src="./img/data/cauldron-concrete.png" /></div></div>
+                <div class="row pack-row"><div class="wide-cell"><img src="./img/data/cauldron-mud.png" /></div></div>
+                <div class="row pack-row"><div class="wide-cell"><img src="./img/data/classic-fishing-loot.png" /></div></div>
+                <div class="row pack-row"><div class="wide-cell"><img src="./img/data/custom-nether-portals.png" /></div></div>
+                <div class="row pack-row"><div class="wide-cell"><img src="./img/data/double-shulker-shells.png" /></div></div>
+                <div class="row pack-row"><div class="wide-cell"><img src="./img/data/fast-leaf-decay.png" /></div></div>
+                <div class="row pack-row"><div class="wide-cell"><img src="./img/data/graves.png" /></div></div>
+                <div class="row pack-row"><div class="wide-cell"><img src="./img/data/husks-drop-sand.png" /></div></div>
+                <div class="row pack-row"><div class="wide-cell"><img src="./img/data/mini-blocks.png" /></div></div>
+                <div class="row pack-row"><div class="wide-cell"><img src="./img/data/more-mob-heads.png" /></div></div>
+                <div class="row pack-row"><div class="wide-cell"><img src="./img/data/name-colors.png" /></div></div>
+                <div class="row pack-row"><div class="wide-cell"><img src="./img/data/player-head-drops.png" /></div></div>
+                <div class="row pack-row"><div class="wide-cell"><img src="./img/data/silence-mobs.png" /></div></div>
+                <div class="row pack-row"><div class="wide-cell"><img src="./img/data/timber.png" /></div></div>
+                <div class="row pack-row"><div class="wide-cell"><img src="./img/data/unlock-all-recipes.png" /></div></div>
+                <div class="row pack-row"><div class="wide-cell"><img src="./img/data/wandering-trader-announcements.png" /></div></div>
+                <div class="row pack-row"><div class="wide-cell"><img src="./img/data/wandering-trades.png" /></div></div>
+
+            </div>
+
+
+        </div>
+        <!-- <iframe class="map-banner" src="http://map.partyatalexs.xyz"></iframe> -->
+    </div>
+</body>
+
+</html>
